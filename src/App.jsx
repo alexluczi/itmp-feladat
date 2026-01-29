ea3bab72c1a1caf36c69495fcbd642c0599c499c
Luczi Alex <luczialex@gmail.com>
2026-01-29 12:45:28 +0100

    App.jsx mostmár csak a listát mutatja meg.

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import List from './pages/List.jsx';
-import Mod from './pages/Mod.jsx';
-import Del from './pages/Del.jsx';
-import Update from './pages/Update.jsx';
-import Single from './pages/Single.jsx';
+import List from './pages/List';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/list" element={<List />} />
-        <Route path="/mod" element={<Mod />} />
-        <Route path="/del" element={<Del />} />
-        <Route path="/update" element={<Update />} />
-        <Route path="/single/:id" element={<Single />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <List />;
 }
 
 export default App
